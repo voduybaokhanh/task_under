0ae5ec4aa677ae2d8c1039212d184431acde0fe6
vdbkhanh <khanhvo908@gmail.com>
2026-02-09 18:03:50 +0700

    Update metro.config.js

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,11 +1,11 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-// Add resolver alias for axios to use browser-compatible version
+config.resolver.resolverMainFields = ["react-native", "browser", "main"];
+
 config.resolver.alias = {
-  ...config.resolver.alias,
-  'axios': require.resolve('axios/dist/browser/axios.cjs'),
+  axios: require.resolve("axios/dist/browser/axios.cjs"),
 };
 
 module.exports = config;
